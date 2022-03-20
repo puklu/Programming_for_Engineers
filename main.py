d122eab23a8fe5176d7a88e14b6564234bf858ab
vivek <vivekb126@gmail.com>
2022-03-20 01:47:57 +0100

    almost there

--- main.py
+++ main.py
@@ -25,17 +25,27 @@ def check_if_valid_date(possible_date):
 
 def extract_DM_date(line):
     date = []
+    flag = 0
+    names_of_months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
+                       "November", "December"]
     for word in line:
         if word.endswith("."):
             possible_date = word.split(".")
             # print(possible_date)
             if len(possible_date) == 3:
                 # print(check_if_valid_date(possible_date))
-                if check_if_valid_date(possible_date):
+                if check_if_valid_date(possible_date) :
                     date.append(word)
-    print(date)
+                    flag += 1
+    if flag == 1:
+        date = date[0].split(".")
+        # print(date)
+        month = names_of_months[int(date[1])-1]
+        day = int(date[0])
+        return month, day
 
 def find_closest_valid_number(line, month):
+
 
     months_31 = ["January", "March", "May", "July", "August", "October", "December"]
     months_30 = ["April", "June", "September", "November"]
@@ -44,6 +54,10 @@ def find_closest_valid_number(line, month):
 
     aux = [0] * len(line)
     for i in range(len(line)):
+        if line[i].endswith(","):
+            line[i] = line[i].split(",")[0]
+        if line[i].endswith("."):
+            line[i] = line[i].split(".")[0]
         if line[i] == month:
             aux[i] = 1
 
@@ -60,7 +74,7 @@ def find_closest_valid_number(line, month):
             if month in months_30:
                 if 0 < int(line[j]) < 31:
                     numbers_in_line[j] = line[j]
-            if month == 2:
+            if month == 'February':
                 if 0 < int(line[j]) < 29:
                     numbers_in_line[j] = line[j]
 
@@ -87,19 +101,26 @@ def extract_month(line):
     # print(line)
     flag = 0
     for words in line:
+        if words.endswith(","):
+            words = words.split(",")[0]
+        if words.endswith("."):
+            words = words.split(".")[0]
         if words in names_of_months:
             flag += 1
             month.append(words)
     if flag == 1:
-        # print(month[0], flag)
         valid_day = find_closest_valid_number(line, month[0])
-        return month[0], valid_day
+        # print(valid_day)
+        if int(valid_day) > 0:
+            return month[0], valid_day
 
 def solution(text_lines):
-    for line in text_lines:
-        # extract_DM_date(line)
-        print(extract_month(line))
-        # print(line)
+    for i in range(len(text_lines)):
+        if extract_DM_date(text_lines[i]):
+            print(i+1, extract_DM_date(text_lines[i]))
+        if extract_month(text_lines[i]):
+            print(i+1, extract_month(text_lines[i]))
+
 
 
 
@@ -107,14 +128,8 @@ def solution(text_lines):
 if __name__ == '__main__':
 
     no_of_lines = input()
-
     no_of_lines = int(no_of_lines)
-
     text_lines = [[] for i in range(no_of_lines)]
-
-    # print(text_lines)
-
-
 
     for i in range(0, no_of_lines):
         text_lines[i] = input().split()
